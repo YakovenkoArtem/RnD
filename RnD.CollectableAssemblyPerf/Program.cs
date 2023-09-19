41014d1336c79f3f5d491b6d75762197acf90335
Артем Яковенко <ya.a@me.com>
2023-09-19 07:20:22 +0300

    RnD.CollectableAssemblyPerf

---
+++ RnD.CollectableAssemblyPerf/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace RnD.CollectableAssemblyPerf;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Test>();
+    }
+}
---
+++ RnD.CollectableAssemblyPerf/Test.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace RnD.CollectableAssemblyPerf;
+
+[MemoryDiagnoser]
+public class Test
+{
+    public static Type New(AssemblyBuilderAccess aba)
+    {
+        /*
+        В Net Core существует только один app домен. Создание второго и последующих будет падать с ошибкой.
+        Что бы иметь возможность выгружать из памяти сгенерированный код, попробуем использовать Collectable сборки.
+        Run быстрее RunAndCollect в 3 раза почему то. Попробуем выяснить почему.
+        */
+
+        AssemblyName an = new("MyDynamicAssembly");
+        AssemblyBuilder ab = AssemblyBuilder.DefineDynamicAssembly(an, aba);
+        ModuleBuilder mb = ab.DefineDynamicModule("MyDynamicModule");
+        TypeBuilder tb = mb.DefineType("MyClass",
+                                       TypeAttributes.Public | TypeAttributes.Sealed);
+
+        //foreach (Column column in table.Columns)
+        //    NewField(tb, column);
+
+        Type t = tb.CreateType()!;
+        return t;
+    }
+
+    [Benchmark(Baseline = true)]
+    public Type TestCreateNonCollectableClass()
+    {
+        return New(AssemblyBuilderAccess.Run);
+    }
+
+    [Benchmark]
+    public Type TestCreateCollectableClass()
+    {
+        return New(AssemblyBuilderAccess.RunAndCollect);
+    }
+}
